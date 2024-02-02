5c17e41d9e4902166b46bb4d2bc5fc718f4535e1
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-02-02 22:26:48 +0100

    packages installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import {motion} from 'framer-motion';
+import Cart from './features/cart/Cart';
 
 function App() {
-  return <motion.div animate={{}}>HIII</motion.div>;
+  return (
+    <motion.div animate={{}}>
+      <Cart />
+    </motion.div>
+  );
 }
 
 export default App;
