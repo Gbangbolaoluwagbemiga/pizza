1d71eb258a83c11b3ddee0b1ac35c12da3f3af7c
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-02-05 00:21:24 +0100

    Today init

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,5 +1,18 @@
+import CartOverview from '../features/cart/CartOverview';
+import Header from './Header';
+
 function AppLayout() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <h1>Content</h1>
+      </main>
+
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,10 @@ import {Link} from 'react-router-dom';
 function Header() {
   return (
     <div>
-      <Link to="/">Fast React Pizza Co.</Link>
+      <Link to="/">
+        Fast React Pizza Co.
+        <p>Philip</p>
+      </Link>
     </div>
   );
 }
