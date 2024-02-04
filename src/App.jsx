594130549518878b01f04a235a94b433970958c5
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-02-04 23:07:27 +0100

    React router implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import {motion} from 'framer-motion';
+// import {motion} from 'framer-motion';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+]);
 
 function App() {
-  return <motion.div></motion.div>;
+  return <RouterProvider router={router} />;
+  // return <motion.div></motion.div>;
 }
 
 export default App;
