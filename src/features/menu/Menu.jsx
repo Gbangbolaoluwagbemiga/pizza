aa8bd439f70c4f38ef0d015fc06a2c4e03b09c46
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-02-05 23:18:06 +0100

    Pizza data implementation

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,11 +1,16 @@
 import {useLoaderData} from 'react-router-dom';
 import {getMenu} from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 function Menu() {
   const data = useLoaderData();
-  console.log(data);
-
-  return <h1>Menu</h1>;
+  return (
+    <ul>
+      {data.map(pizza => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
 export async function loader() {
