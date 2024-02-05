5c44c465872467ffa7eb2843da88753a7cdac027
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-02-05 23:58:16 +0100

    search order completed

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,15 +1,22 @@
 import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 function SearchOrder() {
   const [query, setQuery] = useState('');
+  const navigate = useNavigate();
   function handleSubmit(e) {
     e.preventDefault();
-    setQuery(e.target.value);
+    if (!query) return;
+    navigate('/order/query');
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" value={query} onChange={handleSubmit} />
+      <input
+        type="text"
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+      />
     </form>
   );
 }
