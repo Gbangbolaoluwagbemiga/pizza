b248cc3a6a320ec9ecca460448b36f2f9e691842
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-02-06 23:46:42 +0100

    styling color

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,4 +1,4 @@
-import {Outlet, useNavigation} from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import CartOverview from '../features/cart/CartOverview';
 import Header from './Header';
 import Loader from './Loader';
@@ -8,7 +8,7 @@ function AppLayout() {
   const isLoading = navigation.state === 'loading';
 
   return (
-    <div className="layout">
+    <div className="layout ">
       {isLoading && <Loader />}
 
       <Header />
