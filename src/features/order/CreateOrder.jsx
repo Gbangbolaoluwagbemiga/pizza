6e47f64ac3c724463599f7b2ee207d2b77dbd2b2
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-02-10 23:06:09 +0100

    style added

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -83,8 +83,9 @@ function CreateOrder() {
           <button
             disabled={isSubmitting}
             className="inline-block rounded-full bg-yellow-400 px-2 py-2 font-semibold
-             uppercase tracking-wide text-stone-800 transition-all duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none
-             focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed lg:px-4 lg:py-3"
+             uppercase tracking-wide text-stone-800 transition-all duration-300 hover:bg-yellow-300
+              focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300
+               focus:ring-offset-2 disabled:cursor-not-allowed lg:px-4 lg:py-3"
           >
             {isSubmitting ? "Placing order" : "Order now"}
           </button>
