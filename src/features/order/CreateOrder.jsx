163f296e836c658c0d87c864ba7b04e41f894a66
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-02-10 23:51:06 +0100

    Button completed

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -84,13 +84,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button
-            disabled={isSubmitting}
-            className="inline-block rounded-full bg-yellow-400 px-2 py-2 font-semibold
-             uppercase tracking-wide text-stone-800 transition-all duration-300 hover:bg-yellow-300
-              focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300
-               focus:ring-offset-2 disabled:cursor-not-allowed lg:px-4 lg:py-3"
-          >
+          <button disabled={isSubmitting}>
             {isSubmitting ? "Placing order" : "Order now"}
           </button>
         </div>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,5 +1,15 @@
-function Button() {
-  return <div></div>;
+function Button({ Children, disabled }) {
+  return (
+    <button
+      disabled={disabled}
+      className="inline-block rounded-full bg-yellow-400 px-2 py-2 font-semibold
+  uppercase tracking-wide text-stone-800 transition-all duration-300 hover:bg-yellow-300
+focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300
+  focus:ring-offset-2 disabled:cursor-not-allowed lg:px-4 lg:py-3"
+    >
+      {Children}
+    </button>
+  );
 }
 
 export default Button;
