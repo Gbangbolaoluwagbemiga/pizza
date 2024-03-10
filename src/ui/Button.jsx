148f69c36abba885626acd4fd9866aa396af84f8
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-03-10 19:54:41 +0100

    Today init

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,10 +2,8 @@ function Button({ children, disabled }) {
   return (
     <button
       disabled={disabled}
-      className="inline-block rounded-full bg-yellow-400 px-2 py-2 font-semibold
-  uppercase tracking-wide text-stone-800 transition-all duration-300 hover:bg-yellow-300
-focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300
-  focus:ring-offset-2 disabled:cursor-not-allowed lg:px-4 lg:py-3"
+      className="inline-block rounded-full bg-yellow-400 px-2 py-2 font-semibold uppercase tracking-wide text-stone-800 transition-all duration-300 hover:bg-yellow-300
+focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed lg:px-4 lg:py-3"
     >
       {children}
     </button>
