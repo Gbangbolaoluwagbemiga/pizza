2c37830ce056f9903622960be8d40af030aca354
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-06-22 18:26:44 +0100

    init

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
-import Username from "../features/user/Username";
+import { Link } from 'react-router-dom'
+import SearchOrder from '../features/order/SearchOrder'
+import Username from '../features/user/Username'
 
 function Header() {
   return (
@@ -11,7 +11,7 @@ function Header() {
       <SearchOrder />
       <Username />
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
