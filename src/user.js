bf062b1408808c33b83c26ca34b4ab9d6cd90468
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-11-21 16:36:08 +0100

    cart reducer stored

--- src/user.js
+++ src/user.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./../src/utils/userSlice";
+import cartReducer from "./../src/features/cart/cartSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    cart: cartReducer,
   },
 });
 
