5bfffa65fd90803cbb6f4ebf123063b36cba6111
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-11-22 00:53:04 +0100

    Redux init

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,24 @@
+import { useDispatch } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import { addItems } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
+
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleBtn() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 2,
+      unitPrice,
+      totalPrice: unitPrice * 2,
+    };
+
+    dispatch(addItems(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -22,7 +38,11 @@ function MenuItem({ pizza }) {
           ) : (
             <p className="font-medium uppercase text-stone-500">Sold out</p>
           )}
-          {!soldOut && <Button type="small">Add to Cart</Button>}{" "}
+          {!soldOut && (
+            <Button type="small" onClick={handleBtn}>
+              Add to Cart
+            </Button>
+          )}{" "}
         </div>
       </div>
     </li>
