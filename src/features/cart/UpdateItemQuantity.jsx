60022ae0c2b80cf4c237958c5c2c0ae21b8be6ac
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-11-24 13:53:52 +0100

    Cart quantity updated

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,15 +1,22 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../../ui/Button";
-import { decreaseItemQuantity, increaseItemQuantity } from "./cartSlice";
+import {
+  decreaseItemQuantity,
+  getCart,
+  increaseItemQuantity,
+} from "./cartSlice";
 
 function UpdateItemQuantity({ id }) {
   const dispatch = useDispatch();
+
+  const cart = useSelector(getCart);
+  const cartItem = cart.find((item) => item.pizzaId === id);
   return (
     <div className="flex items-center gap-1 md:gap-3">
       <Button type={"round"} onClick={() => dispatch(decreaseItemQuantity(id))}>
         -
       </Button>
-      <p></p>
+      <p>{cartItem?.quantity}</p>
       <Button type={"round"} onClick={() => dispatch(increaseItemQuantity(id))}>
         +
       </Button>
