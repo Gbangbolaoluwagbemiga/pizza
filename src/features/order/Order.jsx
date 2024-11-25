f19b8f03f3fd95da5a5a57e872a732df88561aa3
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-11-25 23:31:02 +0100

    Fetcher init

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -64,7 +64,15 @@ function Order() {
 
       <ul className="dive-stone-200 divide-y border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === "loading"}
+            ingredients={
+              fetcher?.data.find((el) => el.id === item.pizzaId).ingredients ??
+              []
+            }
+          />
         ))}
       </ul>
 
--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -4,7 +4,7 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li>
+    <li className="space-y-1">
       <div className="flex items-center justify-between py-2">
         <p>
           <span className="font-semibold">{quantity}&times;</span>{" "}
@@ -12,6 +12,9 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
         </p>
         <p className="font-medium">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className="text-sm">
+        {isLoadingIngredients ? "Loading..." : ingredients.join(", ")}
+      </p>
     </li>
   );
 }
